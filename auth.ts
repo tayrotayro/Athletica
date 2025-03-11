73b80103674b19742d3938628c53f2e345322556
Taylor Rotolo <rotolotaylor@gmail.com>
2025-03-11 13:52:54 -0500

    edit jwt token

--- auth.ts
+++ auth.ts
@@ -55,6 +55,8 @@ export const config = {
     async session({ session, user, trigger, token }: any) {
       // set user ID from token
       session.user.id = token.sub
+      session.user.role = token.role
+      session.user.name = token.name
       
       // if there is an update, set the user name
       if (trigger === 'update') {
@@ -62,7 +64,25 @@ export const config = {
       }
 
       return session
-    }
+    },
+    async jwt({ token, user, trigger, session}: any) {
+      // assign user fields to token
+      if (user) {
+        token.role = user.role
+
+        // if user has no name then use email
+        if (user.name === 'NO_NAME') {
+          token.name = user.email!.split('@')[0]
+
+          // update database to reflect token name
+          await prisma.user.update({
+            where: { id: user.id },
+            data: { name: token.name }
+          })
+        }
+      }
+      return token;
+    },
   }
 } satisfies NextAuthConfig
 
