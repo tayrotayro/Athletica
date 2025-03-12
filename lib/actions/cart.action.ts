6bf8d62f42624f113b88aea763312093f4c6fceb
Taylor Rotolo <rotolotaylor@gmail.com>
2025-03-11 20:11:07 -0500

    disable no unused vars

--- lib/actions/cart.action.ts
+++ lib/actions/cart.action.ts
@@ -1,6 +1,7 @@
 'use server'
 import { CartItem } from "@/types"
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function addItemToCart(data: CartItem) {
   return {
     success: false,
